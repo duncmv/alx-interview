e70faad310b41d635ba78aa1f3c8d75b53d8c8aa
duncmv <dtindiwensi@gmail.com>
2024-06-04 11:30:56 +0300

    tasks

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,15 +3,20 @@
 
 
 def canUnlockAll(boxes):
-  """determines if all the boxes can be opened"""
-  box_number = len(boxes)
-  if box_number < 2:
-    return True
-  keys = boxes[0]
-  if 1 not in keys:
-    return False
-  for i in range(1, box_number - 1):
-    keys.extend(boxes[i])
-    if i + 1 not in keys:
-      return False
-  return True
+    """determines if all the boxes can be opened"""
+    box_number = len(boxes)
+    if box_number < 2:
+        return True
+    keys = set(boxes[0])
+    open = set([0])
+    if 1 not in keys:
+        return False
+    while True:
+        if len(open) == box_number:
+            return True
+        for i in open:
+            open = open.union(set(boxes[i]))
+            keys = keys.union(set(boxes[i]))
+        open = open.difference(keys)
+        if not open:
+            return False
